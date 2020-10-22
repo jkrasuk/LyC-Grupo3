f85a30d96ad601907a011f4f3ad35fe9a328a991
seback95 <38386838+seback95@users.noreply.github.com>
2020-10-22 07:24:39 -0300

    Update tercetos.h

--- Entrega 2/tercetos.h
+++ Entrega 2/tercetos.h
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
-#include "tabla_simbolos.h"
+#include "tools.h"
 
 /*
 Esto permite manejarnos mejor con los tipos de los tercetos antes que tener un define para cada uno.
@@ -15,6 +15,7 @@ typedef enum tipoTerceto {
     esMultiplicacion,
     esDivision,
     esComparacion,
+    esDesconocido
 } tipoTerceto;
 
 /*  Esta estructura es para identificar el tipo de elemento en el terceto:
@@ -22,8 +23,8 @@ typedef enum tipoTerceto {
 	ind: El elemento del terceto es un indice a otro terceto.	
 */
 typedef struct  unionTerceto {
-    char* cad;
-    int ind;
+    char* cad; //puntero al lexema en la ts.
+    int ind;   //indice a un terceto en el array global de tercetos.
 } unionTerceto;
 
 /*  Esta struct es necesaria para poder determinar que tipo de 
@@ -38,4 +39,206 @@ typedef struct elemento {
 typedef struct terceto {
     elemento elementos[3];
     tipoTerceto tipoTerc;
+    /*Este tipo va a servir para validar compatibilidad de los datos en las operaciones. Vendria a hacer el "Tipo de valor del resultado".*/
+    tipoValor tipoVal;
 } terceto;
+
+elemento crearElemStr(const char*);
+elemento crearElemInt(int);
+indice crearTerceto(elemento, elemento, elemento, tipoValor, tipoTerceto);
+indice crearTercetoOperacion(const char* op, indice ind1, indice ind2);
+indice crearTercetoAsignacion(indice ind1, indice ind2);
+tipoValor obtenerTipoSimbolo(char * tipo);
+void imprimirTercetos();
+
+/* Índice global para tercetos */
+int indTercetos = 0;
+/* Array de Tercetos */
+terceto tercetos[900];
+
+
+elemento crearElemStr(const char* str) {
+    elemento e;
+    e.valor.cad = strdup(str);
+    e.tipo = string;
+    return e;
+}
+
+elemento crearElemInt(int ind) {
+    elemento e;
+    e.valor.ind = ind;
+    e.tipo = entero;
+    return e;
+}
+/*  Crear un terceto con los elementos pasados por parámetro y se Agregamos
+    al array global de tercetos. Esta es una función genérica para insertar
+    un terceto. */
+indice crearTerceto(elemento e1, elemento e2, elemento e3, tipoValor tipoV, tipoTerceto tipoT) {
+    terceto t;
+    indice ind;
+
+    t.elementos[0] = e1;
+    t.elementos[1] = e2;
+    t.elementos[2] = e3;
+    t.tipoVal = tipoV;
+    t.tipoTerc = tipoT;
+
+    /* Agregamos el terceto al array global de tercetos */
+    tercetos[indTercetos] = t;
+    ind.tipo = esTerceto;
+    ind.datoind.indiceTerceto = indTercetos;
+    indTercetos++;
+
+    /* Devolvemos el índice que apunta al terceto creado */
+    return ind;
+}
+/*  Crear un terceto, donde el primer elemento es una string con el valor
+    de alguna operación (ejemplo, "+") y los otros 2 son valores int que
+    corresponden a los índices de los tercetos sobre los que se realiza
+    la operación. */
+
+indice crearTercetoOperacion(const char* op, indice ind1, indice ind2) {
+    elemento elem1, elem2;
+    tipoValor tipo1, tipo2, tipoResultado;
+    tipoTerceto tipoT;
+
+    if (ind1.tipo == esTerceto) {
+        elem1 = crearElemInt(ind1.datoind.indiceTerceto);
+        tipo1 = tercetos[ind1.datoind.indiceTerceto].tipoVal;
+    } else { /* El índice es de un símbolo */
+        elem1 = crearElemStr(ind1.datoind.punteroSimbolo->lexema);
+        tipo1 = obtenerTipoSimbolo(ind1.datoind.punteroSimbolo->tipo);
+    }
+
+    if (ind2.tipo == esTerceto) {
+        elem2 = crearElemInt(ind2.datoind.indiceTerceto);
+        tipo2 = tercetos[ind2.datoind.indiceTerceto].tipoVal;
+    } else {
+        elem2 = crearElemStr(ind2.datoind.punteroSimbolo->lexema);
+        tipo2 = obtenerTipoSimbolo(ind2.datoind.punteroSimbolo->tipo);
+    }
+
+    /* Validamos que los tipos de la expresión sean compatibles (esto hay que definirlo entre todos por ahora yo tome este criterio de validación)*/
+    if (tipo1 == tipo2) {
+        tipoResultado = tipo1;
+    } else if ((tipo1 == real && tipo2 == entero) || (tipo1 == entero && tipo2 == real)) { //lo pensé como los lenguajes de alto nivel que si sumas real a entero suma a la parte entera del real como corresponde.
+        tipoResultado = real;
+    } else {
+        printf("\nError Error en la linea %d: La operacion %s con esos tipos de datos no es compatible.", 
+                yylineno, op, nombreTiposVal[tipo1], nombreTiposVal[tipo2]);
+        exit(1);
+    }
+     /* Seteamos el tipo de operación */
+    switch (op[0]) {
+        case '+':
+            tipoT = esSuma;
+            break;
+        case '-':
+            tipoT = esResta;
+            break;
+        case '*':
+            tipoT = esMultiplicacion;
+            break;
+        case '/':
+            tipoT = esDivision;
+            break;
+        default:
+            tipoT = esDesconocido;
+            break;
+    }
+
+    return crearTerceto(crearElemStr(op), elem1, elem2, tipoResultado, tipoT);
+}
+/*  Crear un terceto, donde el primer y segundo elementos son indices de 
+    tercetos. Crea una operación de asignación ("=", ind1, ind2) pero antes
+    valida que la asignación pueda realizarse */
+indice crearTercetoAsignacion(indice ind1, indice ind2) {
+    elemento elem1, elem2;
+    tipoValor tipo1, tipo2;
+
+    if (ind1.tipo == esTerceto) {
+        elem1 = crearElemInt(ind1.datoind.indiceTerceto);
+        tipo1 = tercetos[ind1.datoind.indiceTerceto].tipoVal;
+    } else { /* El índice es de un símbolo */
+        elem1 = crearElemStr(ind1.datoind.punteroSimbolo->lexema);
+        tipo1 = obtenerTipoSimbolo(ind1.datoind.punteroSimbolo->tipo);
+    }
+
+    if (ind2.tipo == esTerceto) {
+        elem2 = crearElemInt(ind2.datoind.indiceTerceto);
+        tipo2 = tercetos[ind2.datoind.indiceTerceto].tipoVal;
+    } else {
+        elem2 = crearElemStr(ind2.datoind.punteroSimbolo->lexema);
+        tipo2 = obtenerTipoSimbolo(ind2.datoind.punteroSimbolo->tipo);
+    }
+
+    /*  La razón por la cuál se necesitó hacer una función aparte solo para las
+        asignaciones es porque la validación de tipo es diferente a la que se hace 
+        en crearTercetoOperación. */
+    if (tipo1 == tipo2 || (tipo1 == real && tipo2 == entero && tipo1 != constante )) { 
+        return crearTerceto(crearElemStr("="), elem1, elem2, tipo1, esAsignacion);
+    } else {
+        printf("\nError en la linea %d: Se intento asignar un %s a una variable de tipo %s.", 
+                yylineno, nombreTiposVal[tipo2], nombreTiposVal[tipo1]);
+        exit(1);
+    }
+}
+
+void imprimirTercetos() {
+   printf("Generando archivo intermedia.txt...\n");
+    FILE *gci;
+    int i, j;
+
+    if ((gci = fopen("intermedia.txt", "w")) == NULL) {
+        printf("No se puede crear el archivo de la lista de tercetos.");
+        exit(1);
+    }
+
+    fprintf(gci, "\n--- TERCETOS ---\n\n");
+    for (i = 0; i < indTercetos; i++) {
+        terceto t = tercetos[i];
+        fprintf(gci, "%d: (", i + 1);
+
+        for (j = 0; j < 3; j++) {
+            elemento e = t.elementos[j];
+
+            switch (e.tipo) {
+                case string:
+                    fprintf(gci, "%s", e.valor.cad);
+                    break;
+                case entero: /* Si es entero, es un índice y le sumamos 1 al mostrarlo */
+                    fprintf(gci, "[%d]", e.valor.ind + 1);
+                    break;
+                default:
+                    fprintf(gci, "_");
+            }
+
+            if (j < 2) {
+                fprintf(gci, ", ");
+            } 
+        }
+        fprintf(gci, ")");
+        fprintf(gci, "\n");
+    }
+    fprintf(gci,"\n--- TERCETOS ---\n");
+    
+    if (fclose(gci) != 0) {
+        printf("No se puede CERRAR el archivo de la lista de tercetos.");
+        exit(1);
+    }
+}
+tipoValor obtenerTipoSimbolo(char * tipo){
+
+        if(strcmp(tipo, "STRING")==0){
+           return string; 
+        }
+        if(strcmp(tipo, "INTEGER")==0){
+           return entero; 
+        }
+        if(strcmp(tipo, "FLOAT")==0){
+           return real; 
+        }
+        if(strcmp(tipo, "CONST")==0){
+           return constante; 
+        }
+}
